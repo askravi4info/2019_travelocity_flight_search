b510aa940b970f32dabfa084accf12941a3ef9ac
askravi4info <askravi4info@gmail.com>
2019-02-04 21:06:58 -0500

    Modified the framework

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,4 +2,6 @@ require 'rspec'
 require 'page-object'
 require 'data_magic'
 
+$url = 'www.travelocity.com'
+
 World(PageObject::PageFactory)
--- features/support/pages/travelocity_home_page.rb
+++ features/support/pages/travelocity_home_page.rb
@@ -4,7 +4,7 @@ class TravelocityHomePage
   include PageObject
   include Utilities
 
-  page_url 'www.travelocity.com'
+  page_url url
 
   link(:select_flight_tab, :id => 'primary-header-flight')
   label(:select_round_trip_option, :id => 'flight-type-roundtrip-label-flp')
